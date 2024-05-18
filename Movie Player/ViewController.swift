edaecae47f4e11f9d11df7c4736f6a54f1a3a98c
faustino <faustinopulido830@gmail.com>
2024-05-18 15:20:08 -0500

    Initial Commit

---
+++ Movie Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Player
+//
+//  Created by Faustino Pulido Sarmiento on 18/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
